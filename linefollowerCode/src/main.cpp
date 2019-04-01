03752684327978743b2fc25dd990b480dd82f69d
Mikael Tulldahl <mikael.tulldahl@cpacsystems.se>
2019-04-01 20:19:11 +0200

    Motor driver class

--- linefollowerCode/src/main.cpp
+++ linefollowerCode/src/main.cpp
@@ -1,22 +1,22 @@
 #include <Arduino.h>
-#include "motors.h"
+#include "Motor.h"
 
 int led = 13;
 
-//The setup function is called once at startup of the sketch
+Motor rightMotor(0);
+Motor leftMotor(1);
+
 void setup()
 {
-// Add your initialization code here
 	pinMode(led, OUTPUT);
-	initMotors();
 }
 
-// The loop function is called in an endless loop
 void loop()
 {
 	digitalWrite(led, HIGH);   // turn the LED on (HIGH is the voltage level)
 	delay(1000);               // wait for a second
 	digitalWrite(led, LOW);    // turn the LED off by making the voltage LOW
 	delay(1000);               // wait for a second
-	setRightMotor(0);
+	rightMotor.set(0.3);
+	leftMotor.set(0.3);
 }
